df9feeaec967441ed9ea53fa854b5fbdf7675873
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-18 10:53:22 -0400

    Update app to fetch data and pass down with outletContext

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,9 @@
-import { useState, useEffect } from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useOutletContext } from "react-router-dom"
 import UserCard from "../components/UserCard"
 
 function Home() {
-  const [users, setUsers] = useState([])
-
-  useEffect(() => {
-    fetch("http://localhost:4000/users")
-      .then((r) => r.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error(error))
-  }, [])
-
-  const userList = users.map((user) => {
-    return <UserCard key={user.id} user={user} />
-  })
+  const users = useOutletContext()
+  const userList = users.map((user) => <UserCard key={user.id} user={user} />)
 
   return (
     <>
